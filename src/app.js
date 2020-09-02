c80f196976c6c365f2d7578df54c42678cf244a7
Abraham Esparza <abrahamesparza.us@gmail.com>
2020-09-01 22:38:53 -0700

    add minimal styling, displays results on client

--- src/app.js
+++ src/app.js
@@ -60,16 +60,22 @@ class App extends React.Component {
     this.findBreweries();
   }
 
+  clearQuery() {
+    let { query } = this.state;
+    if (query === '') {
+      this.setState({
+        matched: []
+      });
+    }
+  }
+
   render() {
     let { query, searchBy, breweries} = this.state;
-    console.log(query);
-    console.log('searchBy', searchBy);
-    console.log('matched:', breweries);
-
     return (
-      <div className='pa4 bg-moon-gray'>
-        <h1 className='tc'>Brewery Finder</h1>
-        <Search change={this.handleChange} click={this.handleClick} search={this.findBreweries} />
+      <div className='pa3 bg-moon-gray'>
+        <h1 className='tc f1'>Brewery Finder</h1>
+        <Search change={this.handleChange} click={this.handleClick} search={this.findBreweries}/>
+        <BreweryList brews={breweries}/>
       </div>
     )
   }
@@ -79,7 +85,7 @@ class App extends React.Component {
 const Search = props => {
   let options = ['City', 'Zipcode']
   return (
-    <div className='mh7'>
+    <div className='mh7 cover'>
     <input type='text' className='w-80 tc' placeholder='Search by city or zipcode' onChange={props.change}/>
       <select className='w-20 tc' onChange={props.click} >
         {options.map((op, i) => {
@@ -89,6 +95,24 @@ const Search = props => {
         )})}
       </select><br/>
       <input type='button' className='ph3' value='Search' onClick={props.search}/>
+    </div>
+  )
+}
+
+/* ----------------------------------------------------------------------- */
+//list breweries function
+const BreweryList = props => {
+  return (
+    <div className='fl w-100 pa2 mt5'>
+      <ul className='list'>
+        {props.brews.map(brewery => {
+          return (
+            <a className='link underline-hover washed-blue' key={brewery.id} target='_blank' href={brewery.website_url}>
+              <li className='tc br1 bb b--near-white f2 mh5 grow' key={brewery.id}>{brewery.name}</li>
+            </a>
+          )
+        })}
+      </ul>
     </div>
   )
 }
